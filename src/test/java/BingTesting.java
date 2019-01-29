ad0bec7c590b8b368b05972def85952dbcf3636a
henrychia12 <henry_chia21@hotmail.co.uk>
2019-01-29 15:31:35 +0000

    drawing a h in paint

--- src/test/java/BingTesting.java
+++ src/test/java/BingTesting.java
@@ -2,36 +2,49 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
 public class BingTesting {
-	
+
 	WebDriver driver;
-	
+
 	@Before
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\chromedriver.exe");
 		driver = new ChromeDriver();
 	}
-	
+
 	@After
 	public void teardown() {
 		driver.quit();
 	}
-	
+
+	@Ignore
 	@Test
-	public void bingSearch() throws InterruptedException{
+	public void bingSearch() throws InterruptedException {
 		driver.get(Constants.URL);
 		BingLandingPage bingLandingPage = PageFactory.initElements(driver, BingLandingPage.class);
 		bingLandingPage.searchBing("selenium");
-		
+
 		BingSearchPage bingSearchPage = PageFactory.initElements(driver, BingSearchPage.class);
 		assertEquals("Selenium not found", "Selenium", bingSearchPage.getSearch());
+	}
+
+	@Test
+	public void drawName() throws InterruptedException {
+		driver.get("https://www.youidraw.com/apps/painter/");
+
+		Actions action = new Actions(driver);
+		action.moveByOffset(300, 500).clickAndHold().moveByOffset(0, 150).moveByOffset(0, -75).moveByOffset(75, 0).moveByOffset(0, 75).release().perform();
+		action.moveByOffset(100, 0).clickAndHold().moveByOffset(0, 150).moveByOffset(0, -75).moveByOffset(75, 0).moveByOffset(0, 75).release().perform();
+		Thread.sleep(10000);
 	}
 
 }
