98dff260d8678eda77670c2659a94ec2f3e8032c
henrychia12 <henry_chia21@hotmail.co.uk>
2019-01-29 16:50:30 +0000

    implicit & explicit wait

--- src/test/java/BingTesting.java
+++ src/test/java/BingTesting.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -10,6 +12,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BingTesting {
 
@@ -37,6 +41,7 @@ public class BingTesting {
 		assertEquals("Selenium not found", "Selenium", bingSearchPage.getSearch());
 	}
 
+	@Ignore
 	@Test
 	public void drawName() throws InterruptedException {
 		driver.get("https://www.youidraw.com/apps/painter/");
@@ -45,6 +50,25 @@ public class BingTesting {
 		action.moveByOffset(300, 500).clickAndHold().moveByOffset(0, 150).moveByOffset(0, -75).moveByOffset(75, 0).moveByOffset(0, 75).release().perform();
 		action.moveByOffset(100, 0).clickAndHold().moveByOffset(0, 150).moveByOffset(0, -75).moveByOffset(75, 0).moveByOffset(0, 75).release().perform();
 		Thread.sleep(10000);
+	}
+	
+	@Ignore
+	@Test
+	public void implicitWait() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://christophperrins.github.io/TestingSite/");
+		WebElement myDynamicElement = driver.findElement(By.xpath("//*[@id=\"quote\"]/h2"));
+		assertEquals("Found you", "-The Shafeeq", myDynamicElement.getText());
+		Thread.sleep(2000);
+	}
+	
+	@Ignore
+	@Test
+	public void explicitWait() throws InterruptedException {
+		driver.get("https://christophperrins.github.io/TestingSite/");
+		WebElement myDynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"quote\"]/h2")));
+		assertEquals("-The Shafeeq", myDynamicElement.getText());
+		Thread.sleep(2000);
 	}
 
 }
